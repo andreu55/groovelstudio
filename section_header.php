be1974ea13109b54ac02bb835c9dbe41043bfb95
Andreu García <anduwet2@gmail.com>
2019-03-30 03:44:32 +0100

    bootstrap 3.3.7

--- section_header.php
+++ section_header.php
@@ -18,7 +18,7 @@
 
   <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 
-  <link rel="stylesheet" href="styles/vendor/bootstrap.css">
+  <link rel="stylesheet" href="styles/vendor/bootstrap.min.css">
   <link rel="stylesheet" href="styles/vendor/font-awesome.min.css">
   <link rel="stylesheet" href="styles/vendor/superslides.css">
   <link rel="stylesheet" href="styles/vendor/owl.carousel.css">
